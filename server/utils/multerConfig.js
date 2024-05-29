687c9aadef30e642b5cad6d08e51270adb1a7518
nazat mustag <nazat.mf@gmail.com>
2024-05-29 11:32:49 +0600

    fix multer config

--- server/utils/multerConfig.js
+++ server/utils/multerConfig.js
@@ -1,9 +1,16 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadsDir = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    return cb(null, "./uploads");
+    return cb(null, uploadsDir);
   },
   filename: function (req, file, cb) {
     const newName = file.originalname.split(".")[0] + "-" + Date.now();
